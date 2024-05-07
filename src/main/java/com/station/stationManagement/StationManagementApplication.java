626640caa237b16fdf0782e92a7ca58e11f2f2cf
Sarobidy <sarobidy.rakotonarivo.1@gmail.com>
2024-05-07 17:05:29 +0300

    first commit

---
+++ src/main/java/com/station/stationManagement/StationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.station.stationManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationManagementApplication.class, args);
+	}
+
+}
